119fca21ea800c0a949f88ae14e8823976aef0ae
Luan <1702282A@umich.mx>
2025-08-31 00:37:43 -0600

    pseudocódigo

---
+++ si.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // char num [] = "hh";
+    // int num2 =atoi(num);
+
+    int num2 = NULL;
+    printf("num: %d\n",num2);
+
+    return 0;
+
+
+}
+
+
+
+
+
+
+
+
